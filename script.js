33452d56527da7011c9007188587511af4efaba2
Torazo5 <torazo.tokuda@gmail.com>
2023-10-21 23:35:22 +0800

    linking and icons for the school

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function addEmptyLines(count) {
     for (let i = 0; i < count; i++) {
         const emptyLine = document.createElement('div');
         emptyLine.className = 'empty-line'; // Optionally, you can add a class for styling
+        emptyLine.style.backgroundColor = '#000'; // Set the background color to black
         document.body.appendChild(emptyLine);
     }
 }
+
 // Call the function to add 10 empty lines
 addEmptyLines(10);
